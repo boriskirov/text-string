0b6d69ecec2c9bc03933d6a229a63540863be7c6
Boris Kirov <borisasenovkirov@gmail.com>
2022-06-02 12:03:39 +0200

    added dependency

--- code.ts
+++ code.ts
@@ -429,6 +429,51 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+    //Update random dependencyy
+    if (msg.type === "change-dependency") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const dependency = [
+            "elasticsearch",
+            "postgresql",
+            "redis",
+            "sqlite",
+            "h2",
+            "h2",
+            "h2/OPBEANS",
+            "kibana",
+            "adminconsole-backend",
+            "metering",
+            "allocator",
+            "gcp-reporter",
+            "constructor",
+            "container-task-service",
+            "zookeeper",
+            "route-server",
+            "client-observer",
+          ];
+          const randomDependency = Math.floor(
+            Math.random() * dependency.length
+          );
+
+          node.characters = dependency[randomDependency];
+        }
+      }
+    }
   };
 } else if (figma.command == "health") {
   async function createHealth(): Promise<string | undefined> {
@@ -1185,6 +1230,80 @@ if (figma.command == "open-plugin") {
   }
 
   createMonitor().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "dependency") {
+  async function createDependency(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const service = [
+            "elasticsearch",
+            "postgresql",
+            "redis",
+            "sqlite",
+            "h2",
+            "h2",
+            "h2/OPBEANS",
+            "kibana",
+            "adminconsole-backend",
+            "metering",
+            "allocator",
+            "gcp-reporter",
+            "constructor",
+            "container-task-service",
+            "zookeeper",
+            "route-server",
+            "client-observer",
+          ];
+          const randomService = Math.floor(Math.random() * service.length);
+
+          node.characters = service[randomService];
+        }
+      }
+    }
+  }
+
+  createDependency().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
