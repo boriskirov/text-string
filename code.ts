7031a50bdd3eba6410fe2fc83544b6f41ebd92c4
Boris Kirov <borisasenovkirov@gmail.com>
2022-06-03 16:37:42 +0200

    added error message and last seen

--- code.ts
+++ code.ts
@@ -474,6 +474,81 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+    //Update random error message
+    if (msg.type === "change-error-message") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const errorMesssage = [
+            "AssertionError: Bad luck!",
+            "ValueError: Cannot use None as a query value",
+            "Page Not Found: ['http://opbeans:3000/api/types/3']",
+            "Page Not Found: ['http://opbeans:3000/api/types/3']",
+            "JSONDecodeError: Expecting value: line 1 column 1 (char 0)",
+            "OperationalError: connection to server at (10.40.6.217)",
+            "Couldn't find Customer with 'id'=601",
+            "Couldn't find Order with 'id'=178",
+            "Couldn't find Customer without an ID",
+            "Response status 404execution expired",
+          ];
+          const randomErrorMessage = Math.floor(
+            Math.random() * errorMesssage.length
+          );
+
+          node.characters = errorMesssage[randomErrorMessage];
+        }
+      }
+    }
+    //Update random last seen
+    if (msg.type === "change-last-seen") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const characters = "123456789";
+          function generateLastSeen(length) {
+            let result = "";
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+              result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+              );
+            }
+            return result;
+          }
+
+          const time = ["days", "hours", "minutes"];
+          const randomLastSeen = Math.floor(Math.random() * time.length);
+          node.characters =
+            generateLastSeen(1) + time[randomLastSeen] + "days ago";
+        }
+      }
+    }
   };
 } else if (figma.command == "health") {
   async function createHealth(): Promise<string | undefined> {
@@ -1304,6 +1379,143 @@ if (figma.command == "open-plugin") {
   }
 
   createDependency().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "error-message") {
+  async function createErrorMessage(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const errorMessage = [
+            "AssertionError: Bad luck!",
+            "ValueError: Cannot use None as a query value",
+            "Page Not Found: ['http://opbeans:3000/api/types/3']",
+            "Page Not Found: ['http://opbeans:3000/api/types/3']",
+            "JSONDecodeError: Expecting value: line 1 column 1 (char 0)",
+            "OperationalError: connection to server at (10.40.6.217)",
+            "Couldn't find Customer with 'id'=601",
+            "Couldn't find Order with 'id'=178",
+            "Couldn't find Customer without an ID",
+            "Response status 404execution expired",
+          ];
+          const randomErrorMessage = Math.floor(
+            Math.random() * errorMessage.length
+          );
+
+          node.characters = errorMessage[randomErrorMessage];
+        }
+      }
+    }
+  }
+
+  createErrorMessage().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "last-seen") {
+  async function createLastSeen(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const characters = "123456789";
+          function generateLastSeen(length) {
+            let result = "";
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+              result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+              );
+            }
+            return result;
+          }
+
+          const time = ["days", "hours", "minutes"];
+          const randomLastSeen = Math.floor(Math.random() * time.length);
+          node.characters =
+            generateLastSeen(1) + " " + time[randomLastSeen] + " " + "ago";
+        }
+      }
+    }
+  }
+
+  createLastSeen().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
