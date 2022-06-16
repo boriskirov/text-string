1cfe8f23cf43a926b05937eb90af0ae565c589c7
Boris Kirov <borisasenovkirov@gmail.com>
2022-06-16 09:52:15 +0200

    added transactions

--- code.ts
+++ code.ts
@@ -549,6 +549,55 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+
+    //Update random transaction
+    if (msg.type === "change-transaction") {
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const transaction = [
+            "ProductDetail",
+            "home",
+            "BlogDetail",
+            "VideoDetail",
+            "downloadsRedesign",
+            "DefaultDetail",
+            "ProductVersion",
+            "PastReleases1",
+            "Click - a",
+            "BlogPost",
+            "Forms",
+            "Click - span-reporter",
+            "fullWidthOverview",
+            "searchPage",
+            "PricingRedesign",
+            "Click - div",
+            "Subscriptions",
+            "FullWidthOverview",
+            "Careers",
+            "CourseDetail",
+            "Blog",
+          ];
+          const randomTransaction = Math.floor(
+            Math.random() * transaction.length
+          );
+
+          node.characters = transaction[randomTransaction];
+        }
+      }
+    }
   };
 } else if (figma.command == "health") {
   async function createHealth(): Promise<string | undefined> {
@@ -1516,6 +1565,86 @@ if (figma.command == "open-plugin") {
   }
 
   createLastSeen().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "transaction") {
+  async function createTransaction(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const transaction = [
+            "ProductDetail",
+            "home",
+            "BlogDetail",
+            "VideoDetail",
+            "downloadsRedesign",
+            "DefaultDetail",
+            "ProductVersion",
+            "PastReleases1",
+            "Click - a",
+            "BlogPost",
+            "Forms",
+            "Click - span-reporter",
+            "fullWidthOverview",
+            "searchPage",
+            "PricingRedesign",
+            "Click - div",
+            "Subscriptions",
+            "FullWidthOverview",
+            "Careers",
+            "CourseDetail",
+            "Blog",
+          ];
+          const randomTransaction = Math.floor(
+            Math.random() * transaction.length
+          );
+
+          node.characters = transaction[randomTransaction];
+        }
+      }
+    }
+  }
+
+  createTransaction().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
