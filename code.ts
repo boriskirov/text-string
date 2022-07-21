2fbfe414c9316bdf1de2cf67ce2bf792382fbfd8
Boris Kirov <borisasenovkirov@gmail.com>
2022-07-21 16:07:51 +0200

    added code font support for error message and storage-size field

--- code.ts
+++ code.ts
@@ -629,6 +629,40 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+
+    if (msg.type === "change-storage-size") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const characters = "123456789";
+          function generateSize(length) {
+            let result = "";
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+              result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+              );
+            }
+            return result;
+          }
+
+          node.characters = generateSize(3) + "." + generateSize(1) + " GB";
+        }
+      }
+    }
   };
 } else if (figma.command == "health") {
   async function createHealth(): Promise<string | undefined> {
@@ -1489,6 +1523,22 @@ if (figma.command == "open-plugin") {
       style: "Extra Bold Italic",
     });
     await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+
+    // Roboto Mono load
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Thin" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Light" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Regular" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Medium" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Bold" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Thin Italic" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Italic" });
+    await figma.loadFontAsync({
+      family: "Roboto Mono",
+      style: "Medium Italic",
+    });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Bold Italic" });
+
     // Make sure the selection is a single piece of text before proceeding.
 
     if (figma.currentPage.selection.length === 0) {
@@ -1738,6 +1788,85 @@ if (figma.command == "open-plugin") {
   }
 
   createErrorType().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "storage-size") {
+  async function createSize(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if (figma.currentPage.selection.length === 0) {
+          // if not, show a message
+          figma.closePlugin("Select a text object to convert it");
+          return;
+        }
+        // if it is not a text object, show a message
+        const nodes = figma.currentPage.selection.filter(function (node) {
+          return node.type === "TEXT";
+        }) as Array<TextNode>;
+        if (nodes.length === 0) {
+          figma.closePlugin("Select a text object to convert it");
+          return;
+        }
+        for (const node of figma.currentPage.selection) {
+          if ("characters" in node) {
+            const characters = "123456789";
+            function generateSize(length) {
+              let result = "";
+              const charactersLength = characters.length;
+              for (let i = 0; i < length; i++) {
+                result += characters.charAt(
+                  Math.floor(Math.random() * charactersLength)
+                );
+              }
+              return result;
+            }
+
+            node.characters = generateSize(3) + "." + generateSize(1) + " GB";
+          }
+        }
+      }
+    }
+  }
+
+  createSize().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
