de5f90bb296bd26377e3a2a487307bb1ae8d49d2
Boris Kirov <borisasenovkirov@gmail.com>
2022-09-27 17:34:38 +0200

    android version added

--- code.ts
+++ code.ts
@@ -734,6 +734,64 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+    if (msg.type === "change-android-version") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const AndroidVersion = [
+            "1",
+            "Eclair 2",
+            "Honeycomb 3",
+            "Ice Cream Sandwich 4",
+            "Lollipop 5",
+            "Marshmallow 6",
+            "7",
+            "Oreo 8",
+            "Pie 9",
+            "10",
+            "11",
+            "12",
+            "12L",
+            "13",
+          ];
+          const randomAndroidVersion = Math.floor(
+            Math.random() * AndroidVersion.length
+          );
+
+          const characters = "123456789";
+          function generateAndroidVersion(length) {
+            let result = "";
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+              result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+              );
+            }
+            return result;
+          }
+
+          node.characters =
+            AndroidVersion[randomAndroidVersion] +
+            "." +
+            generateAndroidVersion(1) +
+            "." +
+            generateAndroidVersion(1);
+        }
+      }
+    }
   };
 } else if (figma.command == "health") {
   async function createHealth(): Promise<string | undefined> {
@@ -2089,6 +2147,95 @@ if (figma.command == "open-plugin") {
   }
 
   createDurationSec().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "android-version") {
+  async function createAndroidVersion(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const AndroidVersion = [
+            "1",
+            "Eclair 2",
+            "Honeycomb 3",
+            "Ice Cream Sandwich 4",
+            "Lollipop 5",
+            "Marshmallow 6",
+            "7",
+            "Oreo 8",
+            "Pie 9",
+            "10",
+            "11",
+            "12",
+            "12L",
+            "13",
+          ];
+          const randomAndroidVersion = Math.floor(
+            Math.random() * AndroidVersion.length
+          );
+
+          const characters = "123456789";
+          function generateAndroidVersion(length) {
+            let result = "";
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+              result += characters.charAt(
+                Math.floor(Math.random() * charactersLength)
+              );
+            }
+            return result;
+          }
+
+          node.characters =
+            AndroidVersion[randomAndroidVersion] +
+            "." +
+            generateAndroidVersion(1) +
+            "." +
+            generateAndroidVersion(1);
+        }
+      }
+    }
+  }
+
+  createAndroidVersion().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
