5a200b3a8965687b94762aed63dd9e91d63dfd7f
Boris Kirov <borisasenovkirov@gmail.com>
2023-01-05 10:30:32 +0100

    adding an env data field

--- code.ts
+++ code.ts
@@ -1679,6 +1679,46 @@ if (figma.command == "open-plugin") {
         }
       }
     }
+    //Update random environment
+    if (msg.type === "change-environment") {
+      // Check if something is selected
+      if (figma.currentPage.selection.length === 0) {
+        // if not, show a message
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+      // if it is not a text object, show a message
+      const nodes = figma.currentPage.selection.filter(function (node) {
+        return node.type === "TEXT";
+      }) as Array<TextNode>;
+      if (nodes.length === 0) {
+        figma.notify("Select a text object to convert it", { timeout: 5 });
+        return;
+      }
+
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const envVar = [
+            "production",
+            "testing",
+            "dev",
+            "staging",
+            "2 environments",
+            "8 environments",
+            "preprod",
+            "locale",
+            "Not defined",
+            "default",
+            "https://www.elastic.co",
+            "https://stag.elastic.co",
+          ];
+
+          const randomEnv = Math.floor(Math.random() * envVar.length);
+
+          node.characters = envVar[randomEnv];
+        }
+      }
+    }
   };
 
   // SHORTCUTS LOGIC BELOW
@@ -4561,6 +4601,80 @@ if (figma.command == "open-plugin") {
   }
 
   createDataFieldApm().then((message: string | undefined) => {
+    figma.closePlugin(message);
+  });
+} else if (figma.command == "environment") {
+  async function createEnvironment(): Promise<string | undefined> {
+    // Inter is the font that objects will be created in Figma.
+    // We need to wait for fonts to load before creating text using them.
+    await figma.loadFontAsync({ family: "Inter", style: "Thin" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Light" });
+    await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Extra Bold" });
+    await figma.loadFontAsync({ family: "Inter", style: "Black" });
+    await figma.loadFontAsync({ family: "Inter", style: "Thin Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Light Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Light Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Medium Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Semi Bold Italic" });
+    await figma.loadFontAsync({ family: "Inter", style: "Bold Italic" });
+    await figma.loadFontAsync({
+      family: "Inter",
+      style: "Extra Bold Italic",
+    });
+    await figma.loadFontAsync({ family: "Inter", style: "Black Italic" });
+
+    // Load Roboto Mono font
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Regular" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Bold" });
+    await figma.loadFontAsync({ family: "Roboto Mono", style: "Italic" });
+    // Make sure the selection is a single piece of text before proceeding.
+    if (figma.currentPage.selection.length === 0) {
+      // if not, show a message
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } // if it is not a text object, show a message
+    const nodes = figma.currentPage.selection.filter(function (node) {
+      return node.type === "TEXT";
+    }) as Array<TextNode>;
+    if (nodes.length === 0) {
+      figma.closePlugin("Select a text object to convert it");
+      return;
+    } else {
+      for (const node of figma.currentPage.selection) {
+        if ("characters" in node) {
+          const envVar = [
+            "production",
+            "testing",
+            "dev",
+            "staging",
+            "2 environments",
+            "8 environments",
+            "preprod",
+            "locale",
+            "Not defined",
+            "default",
+            "https://www.elastic.co",
+            "https://stag.elastic.co",
+          ];
+
+          const randomEnv = Math.floor(Math.random() * envVar.length);
+
+          node.characters = envVar[randomEnv];
+        }
+      }
+    }
+  }
+
+  createEnvironment().then((message: string | undefined) => {
     figma.closePlugin(message);
   });
 }
